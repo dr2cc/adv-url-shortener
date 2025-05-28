54ac2679f224eb5f1b24542a0c8fef58aac30df9
Constantine <tmail99@gmail.com>
2025-05-28 14:09:19 +0300

    iter6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,134 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+
+	"adv-url-shortener/internal/config"
+	"adv-url-shortener/internal/http-server/handlers"
+	mwLogger "adv-url-shortener/internal/http-server/middleware/logger"
+	"adv-url-shortener/internal/lib/logger/sl"
+	"adv-url-shortener/internal/storage/sqlite"
+
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+)
+
+// Объявить переменные окружения из iter5 так:
+// $env:SERVER_ADDRESS = "localhost:8089"
+// $env:BASE_URL  = "http://localhost:9999"
+
+// Если использую local.yaml , то перед запуском нужно установить переменную окружения CONFIG_PATH
+//
+// $env:CONFIG_PATH = "C:\__git\adv-url-shortener\config\local.yaml"
+// $env:CONFIG_PATH = "C:\Mega\__git\adv-url-shortener\config\local.yaml"  (на ноуте)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// обрабатываем аргументы командной строки
+	config.ParseFlags()
+
+	cfg := config.MustLoad()
+
+	//
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env)) // к каждому сообщению будет добавляться поле с информацией о текущем окружении
+
+	log.Info("initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
+	log.Debug("logger debug mode enabled")
+
+	//
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID) // Добавляет request_id в каждый запрос, для трейсинга
+	router.Use(middleware.Logger)    // Логирование всех запросов
+	router.Use(middleware.Recoverer) // Если где-то внутри сервера (обработчика запроса) произойдет паника, приложение не должно упасть
+	//переопределяем внутренний логгер
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.URLFormat) // Парсер URLов поступающих запросов
+
+	// // adv #start#
+	// router.Post("/", save.New(log, storage))
+	// // adv #end#
+
+	// // Примитивное хранилище - map
+	// storageInstance := maps.NewStorage()
+
+	// sqlite.New или "подключает" файл db , а если его нет то создает
+	storageInstance, err := sqlite.New("./storage.db")
+	if err != nil {
+		log.Error("failed to initialize storage", sl.Err(err))
+	}
+
+	router.Post("/", handlers.PostHandler(log, storageInstance))
+	router.Get("/{id}", handlers.GetHandler(log, storageInstance))
+
+	// // примитивный запуск сервера
+	//return http.ListenAndServe(config.FlagRunAddr, router)
+
+	// adv #server#
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	srv := &http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+		//ReadTimeout:  cfg.HTTPServer.Timeout,
+		//WriteTimeout: cfg.HTTPServer.Timeout,
+		//IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Error("failed to start server")
+		}
+	}()
+
+	log.Info("server started")
+
+	<-done
+	log.Info("stopping server")
+
+	// TODO: move timeout to config
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server", sl.Err(err))
+
+		return
+	}
+
+	// TODO: close storage
+
+	log.Info("server stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
---
+++ internal/http-server/handlers/handlers.go
@@ -0,0 +1,91 @@
+package handlers
+
+import (
+	"adv-url-shortener/internal/config"
+	"adv-url-shortener/internal/lib/random"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"strings"
+)
+
+const aliasLength = 6
+
+//go:generate go run github.com/vektra/mockery/v2@v2.28.2 --name=URLSaver
+type URLSaver interface {
+	SaveURL(URL, alias string) (int64, error)
+}
+
+// Функция PostHandler уровня пакета handlers
+func PostHandler(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			param, err := io.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			// Преобразуем тело запроса (тип []byte) в строку:
+			url := string(param)
+
+			// // Генерируем короткий идентификатор и создаем запись в нашем хранилище
+			// //config.FlagURL соответствует "http://" + req.Host если не использовать аргументы
+			alias := random.NewRandomString(aliasLength)
+
+			// Объект urlSaver (переданный при создании хендлера из main)
+			// используется именно тут!
+			id, err := urlSaver.SaveURL(url, alias)
+
+			if err != nil {
+				fmt.Println("failed to add url")
+				return
+			}
+			// возвращаем ответ с сообщением об успехе
+			log.Info("url added", slog.Int64("id", id))
+
+			// Устанавливаем статус ответа 201
+			w.WriteHeader(http.StatusCreated)
+			if config.FlagURL == "none" {
+				fmt.Fprint(w, "http://"+r.Host+"/"+alias)
+			} else {
+				fmt.Fprint(w, config.FlagURL+"/"+alias)
+			}
+
+		default:
+			w.Header().Set("Location", "Method not allowed")
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	}
+}
+
+// моя выдумка
+type URLGeter interface {
+	GetURL(alias string) (string, error)
+}
+
+// Функция GetHandler уровня пакета handlers
+func GetHandler(log *slog.Logger, urlGeter URLGeter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			alias := strings.TrimPrefix(r.RequestURI, "/")
+
+			//url, err := maps.GetEntry(us, id)
+			url, err := urlGeter.GetURL(alias)
+			if err != nil {
+				w.Header().Set("Location", err.Error())
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			w.Header().Set("Location", url)
+			w.WriteHeader(http.StatusTemporaryRedirect)
+		default:
+			w.Header().Set("Location", "Method not allowed")
+			w.WriteHeader(http.StatusBadRequest)
+		}
+	}
+}
